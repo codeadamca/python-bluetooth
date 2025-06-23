cd7b778e2c2c67265ec8c9e3da24beaafcafef3b
Adam Thomas <thomasadam83@hotmail.com>
2025-06-22 23:00:35 -0400

    Removed temp files

--- test.py
+++
@@ -1,8 +0,0 @@
-# Test
-# Canada
-# Here
-# Red
-# Blue
-
-rsync -av --exclude='.git' /Users/thomasa/Desktop/CodeAdam/python-bluetooth/ /tmp/python-bluetooth-clean/ 
-scp -r /tmp/python-bluetooth-clean/ robot@ev3dev.local:/home/robot/
